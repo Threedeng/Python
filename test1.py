e082ec9701e9b67c7f9cbba555a8bf269aa4352a
1401182839@qq.com <Deng8155195>
2023-02-22 23:18:14 +0800

    记录一下

--- test1.py
+++ test1.py
@@ -2,6 +2,15 @@
 # -*- coding: UTF-8 -*-
 # 文件名：test1.py
 
-def printhellow():
-    print "I am print hellow Python"
-    return "This is a return"
+
+class test():
+    global sum
+    sum = 'Hello,I am global!'
+    def printhellow(self):
+        print ("I am print hellow Python")
+        print (sum)
+
+
+
+tst = test()
+tst.printhellow()
--- test2.py
+++ test2.py
@@ -1,4 +1,33 @@
+# -*- coding: utf-8 -*-
+import urllib2
+from scrapy.spiders import Spider
 
-from test1 import printthehellow
+from test1 import printhellow
 
-test1.printhellow()
+def transSalary(salary):
+    result = ''
+    if "千" in salary:
+        begnum = float(salary.split("-")[0]) * 1000
+        endnum = float(salary.split('-')[1].split('千')[0]) * 1000
+    elif "万" in salary:
+        begnum = float(salary.split("-")[0]) * 10000
+        endnum = float(salary.split('-')[1].split('万')[0]) * 10000
+    else:
+        return "No wan or qian"
+
+    if '年' in salary:
+        begnum = begnum/12
+        endnum = endnum/12
+
+    result = str(begnum) + '-' + str(endnum)
+    return result,begnum,endnum
+
+
+salary = '3-5千'
+
+print transSalary(salary)
+
+
+
+
+
